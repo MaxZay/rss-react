3983295534942ebad82daf468467dfe291f8943c
MaxZay <maximzay2017@gmail.com>
2021-08-03 18:27:58 +0300

    feat: add search bar

--- components/src/Search.tsx
+++ components/src/Search.tsx
@@ -4,7 +4,11 @@ import LOUPE from './assets/loupe.svg'
 export const Search = () => {
   return (
     <div className="search-item">
-      <input className="search-item__input" type="text"></input>
+      <input
+        className="search-item__input"
+        type="text"
+        placeholder="Search"
+      ></input>
       <button className="search-item__button">
         <img
           className="search-item__loupe"
