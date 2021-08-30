4a51146b58902c867aa50f8a0460e578f5f4d86c
MaxZay <maximzay2017@gmail.com>
2021-08-30 19:42:54 +0300

    feat: add store

--- api/src/index.tsx
+++ api/src/index.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { App } from './components/App'
+import { store } from './store'
 import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
