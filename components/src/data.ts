383f0f83700da2b2f340b144d805dc8354c88ebc
MaxZay <maximzay2017@gmail.com>
2021-09-03 21:21:00 +0300

    fix: moved data to a new file

---
+++ components/src/data.ts
@@ -0,0 +1,82 @@
+import Book from './classes/Book'
+
+export const data = [
+  new Book(
+    'http://loveread.ec/img/photo_books/25721.jpg',
+    'First player ready',
+    '37$',
+    1
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+  new Book(
+    'https://books.google.by/books/publisher/content?id=zTsnDgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71markcggUmrGXyl0wipF04E0qslv5louTXv58_kOFLM7s9QqsOm0WXRMtlaEOZC9mZIi6MpLqO19kaNLEbYO8OhsyBEFzuGZL8t0jMx-y2xz50CImhwSYAZm89cTlC9qQTITcg',
+    'The Last Wish',
+    '7$',
+    2
+  ),
+]
